d6d7f719234734aa94fc8257ae23bfb1c8169b8d
Swapna Badave <swapna.badave@ibm.com>
2026-05-09 17:10:54 +0530

    Add VS Code default Windows shortcuts table to 08_Comments.js

---
+++ Chapter_03_Indentifier_Literals/08_Comments.js
@@ -0,0 +1,27 @@
+// This is a single-line comment
+
+/*
+  VS Code default Windows shortcuts
+
+  Shortcut          | Action
+  ------------------|--------------------------------------
+  Ctrl+Shift+P      | Open Command Palette
+  Ctrl+P            | Quick Open file
+  Ctrl+N            | New file
+  Ctrl+S            | Save file
+  Ctrl+F            | Find in file
+  Ctrl+H            | Replace in file
+  Ctrl+Shift+F      | Search across files
+  Ctrl+`            | Toggle integrated terminal
+  Ctrl+B            | Toggle sidebar visibility
+  Ctrl+K Ctrl+S     | Open Keyboard Shortcuts
+  Ctrl+Space        | Trigger IntelliSense / suggestions
+  Ctrl+/            | Toggle line comment
+  Shift+Alt+A       | Toggle block comment
+  Alt+Up / Alt+Down | Move line up/down
+  F2                | Rename symbol
+  Ctrl+Shift+O      | Go to symbol in file
+  Ctrl+Tab          | Switch editor tabs
+  Ctrl+K Z          | Toggle Zen Mode
+*/
+ 
